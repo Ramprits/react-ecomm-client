89e4d684d8d72220915f1121c56fdd8c3a424e2c
Ramprit Sahani <rampritsahani@outlook.in>
2022-07-16 10:35:22 +0530

    added cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
+import { Refine } from "@pankod/refine-core";
+import routerProvider, {
+  HashRouterComponent,
+} from "@pankod/refine-react-router-v6";
+import { authProvider } from "authProvider";
+import axios from "axios";
+import Products from "components/Products";
+import Cart from "pages/Cart";
+import Home from "pages/Home";
+import Login from "pages/Login";
 import Product from "pages/Product";
+import Register from "pages/Register";
+const axiosInstance = axios.create();
+const TOKEN_KEY = "";
+axiosInstance.interceptors.request.use((request) => {
+  const token = localStorage.getItem(TOKEN_KEY);
+  if (token) {
+    if (request.headers) {
+      request.headers["Authorization"] = `Bearer ${token}`;
+    } else {
+      request.headers = {
+        Authorization: `Bearer ${token}`,
+      };
+    }
+  }
+
+  return request;
+});
 
 function App() {
   return (
-    <>
-      <Product />
-    </>
+    <Refine
+      routerProvider={{
+        ...routerProvider,
+        routes: [
+          {
+            element: <Login />,
+            path: "/login",
+            // layout: true,
+          },
+          {
+            element: <Register />,
+            path: "/register",
+            layout: true,
+          },
+          {
+            element: <Cart />,
+            path: "/cart",
+            layout: true,
+          },
+          {
+            element: <Product />,
+            path: "/product/:id",
+            layout: true,
+          },
+        ],
+        RouterComponent: HashRouterComponent,
+      }}
+      dataProvider={("", axiosInstance)}
+      authProvider={authProvider(axiosInstance)}
+      resources={[
+        {
+          name: "home",
+          list: Home,
+        },
+      ]}
+    />
   );
 }
 
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,32 +1,111 @@
-import styled from "@emotion/styled";
-import Announcemnet from "components/Announcemnet";
-import Footer from "components/Footer";
+import { Add, Remove } from "@mui/icons-material";
 import Navbar from "components/Navbar";
-import Products from "components/Products";
-import React from "react";
+import Announcemnet from "components/Announcemnet";
+import styled from "styled-components";
+import Footer from "components/Footer";
+
 const Container = styled.div``;
-const FilterContainer = styled.div`
+
+const Wrapper = styled.div`
+  padding: 50px;
   display: flex;
-  justify-content: space-between;
-  margin: 20px 20px;
-`;
-const Filter = styled.div`
-  display: flex;
-`;
-const FilterText = styled.h2`
-  font-size: 20px;
 `;
 
-const Select = styled.select`
-  margin-left: 10px;
-  height: 38px;
-  border-color: gray;
-  border-radius: 5px;
-  outline: none;
-  padding: 10px;
+const ImgContainer = styled.div`
+  flex: 1;
 `;
-const Option = styled.option`
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
   font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
 `;
 
 const Product = () => {
@@ -34,35 +113,48 @@ const Product = () => {
     <Container>
       <Navbar />
       <Announcemnet />
-      <FilterContainer>
-        <Filter>
-          <FilterText>Filter Products:</FilterText>
-          <Select>
-            <Option disabled selected>
-              Color
-            </Option>
-            <Option>White</Option>
-            <Option>Black</Option>
-            <Option>Red</Option>
-            <Option>Blue</Option>
-            <Option>Yellow</Option>
-          </Select>
-
-          <Select>
-            <Option disabled selected>
-              Size
-            </Option>
-            <Option>XS</Option>
-            <Option>MD</Option>
-            <Option>LG</Option>
-            <Option>XXL</Option>
-          </Select>
-        </Filter>
-        <Filter>
-          <FilterText>Sort Products:</FilterText>
-        </Filter>
-      </FilterContainer>
-      <Products />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+            tristique tortor pretium ut. Curabitur elit justo, consequat id
+            condimentum ac, volutpat ornare.
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
       <Footer />
     </Container>
   );
